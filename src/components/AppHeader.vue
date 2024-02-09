66da6cdf147561572bf0ac14c64c074f02bba8ec
FrancescoM95 <mon95fran@gmail.com>
2024-02-09 12:37:42 +0100

    header fix

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,7 +9,9 @@ export default {
 <template>
     <header>
         <div class="container d-flex justify-content-center align-items-center h-100">
-            <h1>Boolflix</h1>
+            <a href="">
+                <h1>Boolflix</h1>
+            </a>
             <AppForm />
         </div>
     </header>
@@ -25,5 +27,9 @@ h1 {
     color: #D40000;
     margin-right: 60px;
     font-size: 3rem;
+}
+
+a {
+    text-decoration: none;
 }
 </style>
